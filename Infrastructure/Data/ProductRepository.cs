ffc61cbe1c498a1d39d7531adef86a88e766097b
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-03-22 23:27:02 -0300

    include brands and types

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -19,12 +19,18 @@ namespace Infrastructure.Data
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await this.context.Products.FindAsync(id);
+            return await this.context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p=> p.Id == id);
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         { 
-            return await this.context.Products.ToListAsync();
+            return await this.context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
