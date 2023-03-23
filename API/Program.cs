f3586f670d3ac2ea1bca5c86718e8b08a94a95c2
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-03-22 22:50:38 -0300

    Seed Data

--- API/Program.cs
+++ API/Program.cs
@@ -35,9 +35,11 @@ using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
 var logger = services.GetRequiredService<ILogger<Program>>();
+
 try
 {
     await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
 }
 catch (Exception ex)
 {
