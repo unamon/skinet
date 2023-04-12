ec05d4ee9c730413322882204f0cfd60e133d260
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-04-12 18:00:14 -0300

    Add Lazy Loading

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -13,16 +13,25 @@ export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
   constructor(private http: HttpClient) {}
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
-    if (shopParams.brandId > 0)  params = params.append('brandId', shopParams.brandId);
-    if (shopParams.typeId > 0)   params = params.append('typeId', shopParams.typeId);  
-    if (shopParams.search != "") params = params.append('search', shopParams.search);
+    if (shopParams.brandId > 0)
+      params = params.append('brandId', shopParams.brandId);
+    if (shopParams.typeId > 0)
+      params = params.append('typeId', shopParams.typeId);
+    if (shopParams.search != '')
+      params = params.append('search', shopParams.search);
     params = params.append('sort', shopParams.sort);
     params = params.append('pageIndex', shopParams.pageNumber);
     params = params.append('pageSize', shopParams.pageSize);
-    
-    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params});
+
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {
+      params,
+    });
   }
 
   getBrands() {
