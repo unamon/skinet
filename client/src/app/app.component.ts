2c10a50d3831d4cc1d70fba7673375d6167bb1d1
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-04-12 13:57:08 -0300

    add paging and search

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Pagination } from './models/pagination';
 import { Product } from './models/products';
 
 @Component({
@@ -12,17 +10,10 @@ export class AppComponent implements OnInit{
   title = 'Skinet';
   products: Product[] =[];
 
-  constructor(private http: HttpClient) { 
+  constructor() { 
 
   }
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: response => this.products = response.data, // what to do next
-      error: error => console.log(error), //what to do if there is an error
-      complete: () => {
-        console.log('request complete');
-        console.log("extra statement")
-      }
-    })
+  
   }
 }
