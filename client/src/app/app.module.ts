050bcbe8c4f691073eeb4a339f40f0c19e078e1a
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-04-12 20:55:16 -0300

    add error handling pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -19,9 +19,11 @@ import { HomeModule } from './home/home.module';
     BrowserAnimationsModule, 
     HttpClientModule,
     CoreModule,
-    HomeModule
+    HomeModule,
+  ],  
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass:ErrorInterceptor, multi: true}
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
