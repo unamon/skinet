705e72fdb210043c31790a236ee64c915a1beda9
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-06-22 16:11:06 -0300

    update extension

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -2,22 +2,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities.Identity;
 using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
 {
     public static class IdentityServiceExtensions
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services, 
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services,
             IConfiguration config)
-        { 
-            services.AddDbContext<AppIdentityDbContext>(opt => {
+        {
+            services.AddDbContext<AppIdentityDbContext>(opt =>
+            {
                 opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
             });
 
-         
-           return services;
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                //
+            })
+            .AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddAuthentication();
+            services.AddAuthorization();
+            return services;
         }
     }
 }
