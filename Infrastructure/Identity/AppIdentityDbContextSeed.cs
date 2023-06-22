72800735635f403a7d5625d0678c9a22a94890a4
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-06-22 16:08:18 -0300

    migration & seeding

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUserAsync(UserManager<AppUser> userManager) { 
+            if (!userManager.Users.Any()) { 
+                var user = new AppUser{
+                    DisplayName = "Bob"
+                    , Email = "bob@bobbity"
+                    , UserName = "bot@test.com"
+                    , Adress = new Address { 
+                        FirstName = "Bob"
+                        , LastName = "Bobbert"
+                        , Street = "10 strt"
+                        , City = "newyork"
+                        , State = "NY"
+                        , ZipCode = "90210"
+                    }
+                };
+            await userManager.CreateAsync(user, "Pa$$w0rd");
+            };
+
+        }
+    }
+}
