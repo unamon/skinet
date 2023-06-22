3d9596d30c629a5977043b1211e1b1c4711e882e
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-06-22 16:00:11 -0300

    add identity extensions

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, 
+            IConfiguration config)
+        { 
+            services.AddDbContext<AppIdentityDbContext>(opt => {
+                opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
+            });
+
+         
+           return services;
+        }
+    }
+}
