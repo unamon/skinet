3e562609f562cdc783004208116e40102fa366e9
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-06-29 22:50:31 -0300

    add functional Basket buttons

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { BasketItem } from '../models/basket';
 import { BasketService } from './basket.service';
 
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
-  styleUrls: ['./basket.component.scss']
+  styleUrls: ['./basket.component.scss'],
 })
 export class BasketComponent {
-  constructor(public basketService: BasketService){}
+  constructor(public basketService: BasketService) {}
 
+  incrementQuantity(item: BasketItem) {
+    this.basketService.addItemToBasket(item);
+  }
+  removeItem(id: number, quantity?: number) {
+    this.basketService.removeItemFromBasket(id, quantity);
+  }
 }
