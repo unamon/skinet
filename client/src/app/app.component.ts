28d9b7091cbaa031ff5ecb87c9b88b3e42eee7d3
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-07-09 21:33:03 -0300

    Add persistent login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 import { Product } from './models/products';
 
@@ -11,10 +12,22 @@ export class AppComponent implements OnInit{
   title = 'Skinet';
   products: Product[] =[];
 
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private accountService:AccountService) {}
 
   ngOnInit(): void {
+    this.loadBasket()
+    this.loadCurrentUser()
+  }
+
+  loadBasket()
+  {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) this.basketService.getBasket(basketId);
+  }
+
+  loadCurrentUser()
+  {
+    const token = localStorage.getItem('token');
+    if (token) this.accountService.loadCurrentUser(token).subscribe();
   }
 }
--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from 'src/app/account/account.service';
 import { BasketService } from 'src/app/basket/basket.service';
 import { BasketItem } from 'src/app/models/basket';
 
@@ -8,8 +9,8 @@ import { BasketItem } from 'src/app/models/basket';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
-constructor(public basketService: BasketService) {}
   
+constructor(public basketService: BasketService, public accountService:AccountService) {}
   getCount(items: BasketItem[]) { 
     return items.reduce((sum, item) => sum + item.quantity, 0)
   }
