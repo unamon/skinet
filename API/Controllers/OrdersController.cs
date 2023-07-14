7f5e612dc27b8f1d63eae6188b8f912ed047c9de
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-07-14 20:36:08 -0300

    Add order controller/service

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Errors;
+using API.Extensions;
+using AutoMapper;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class OrdersController : BaseApiController
+    {
+        private readonly IOrderService orderService;
+        private readonly IMapper mapper;
+
+        public OrdersController(IOrderService orderService, IMapper mapper)
+        {
+            this.orderService = orderService;
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(OrderDto orderDto)
+        {
+            var email = HttpContext.User.RetrieveEmailFromPrincipal();
+
+            var address = mapper.Map<AddressDto, Address>(orderDto.ShipToAddress);
+
+            var order = await orderService.CreateOrderASync(email, orderDto.DeliveryMethodId, orderDto.BasketId, address);
+
+            if(order == null) return BadRequest(new ApiResponse(400, "Problem creating order"));
+
+            return Ok(order);
+        }
+    }
+}
--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
 using Core.Entities.OrderAggregate;
 using Core.Interfaces;
 
@@ -9,9 +10,44 @@ namespace Infrastructure.Services
 {
     public class OrderService : IOrderService
     {
-        public Task<Order> CreateOrderASync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress)
+        private readonly IGenericRepository<Order> orderRepo;
+        private readonly IGenericRepository<DeliveryMethod> dmRepo;
+        private readonly IGenericRepository<Product> prodRepo;
+        private readonly IBasketRepository basketRepo;
+
+        public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod> dmRepo,
+        IGenericRepository<Product> prodRepo, IBasketRepository basketRepo)
         {
-            throw new NotImplementedException();
+            this.orderRepo = orderRepo;
+            this.dmRepo = dmRepo;
+            this.prodRepo = prodRepo;
+            this.basketRepo = basketRepo;
+        }
+
+        public async Task<Order> CreateOrderASync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
+        {
+            // get basket from the repo
+            var basket = await basketRepo.GetBasketAsync(basketId);
+            // get items from product repo
+            var items = new List<OrderItem>();
+                foreach(var item in basket.Items)
+                {
+                    var productItem = await prodRepo.GetByIdAsync(item.Id);
+                    var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name,
+                        productItem.PictureUrl);
+                    var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+                    items.Add(orderItem);
+                }
+            // get delivery method from repo
+            var deliveryMethod = await dmRepo.GetByIdAsync(deliveryMethodId);
+            // calc subtotal
+            var subtotal = items.Sum(item => item.Price * item.Quantity);
+            // create order
+            var order = new Order(buyerEmail, shippingAddress, deliveryMethod, items, subtotal);
+            // TODO: save to DB
+            
+            // return the order
+            return order;
         }
 
         public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
